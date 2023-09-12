ce3c3277c2dc6fd437cddb933d8aac5364599e77
Anio-Mazangia <8qs35p@gmail.com>
2023-09-12 13:19:10 +0300

    t

--- week2/day5/miniProjeect/tictac.py
+++ week2/day5/miniProjeect/tictac.py
@@ -20,3 +20,27 @@ def player_input ():
 
 player_input ()
 
+def update_board(row, column, player):
+    if board[row][column] == ' ':
+        board[row][column] = player
+        return True
+    else:
+        return False
+    
+def check_winner():
+
+    for row in board:
+        if row[0] == row[1] == row[2] != ' ':
+            return row[0]
+
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != ' ':
+            return board[0][col]
+
+    if board[0][0] == board[1][1] == board[2][2] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != ' ':
+        return board[0][2]
+
+    return None    
+
